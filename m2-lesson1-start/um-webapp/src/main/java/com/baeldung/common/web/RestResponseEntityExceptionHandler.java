fb37ec2a23d44faf079eac4ed5317caa0ddcd97f
Todd A Nordquist <toddnordquist144@gmail.com>
2020-01-09 14:23:18 -0700

    Todd's initial commit before adding any code

--- m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
+++ m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
@@ -2,7 +2,12 @@ package com.baeldung.common.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
