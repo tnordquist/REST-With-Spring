f30addad12c41c279971db791384333b40401de7
Todd A Nordquist <toddnordquist144@gmail.com>
2020-01-13 13:18:30 -0700

    Added maven dependency: hibernate validator, to pom

--- m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
+++ m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.baeldung.common.web;
 
 import com.baeldung.common.web.exception.ApiError;
+import com.baeldung.common.web.exception.MyBadRequestException;
+import javax.validation.ConstraintViolationException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -44,6 +48,14 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
       final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status,
       final WebRequest request) {
     return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers,
+        HttpStatus.BAD_REQUEST, request);
+  }
+
+
+  @ExceptionHandler(value = {DataIntegrityViolationException.class, MyBadRequestException.class})
+  protected final ResponseEntity<Object> handleBadRequest(final RuntimeException ex,
+      final WebRequest request) {
+    return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), new HttpHeaders(),
         HttpStatus.BAD_REQUEST, request);
   }
 
