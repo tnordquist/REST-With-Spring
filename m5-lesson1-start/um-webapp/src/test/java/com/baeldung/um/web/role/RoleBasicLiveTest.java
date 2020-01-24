cf84d95a8cfd677d42e2a426f6d527037b2443af
Todd A Nordquist <toddnordquist144@gmail.com>
2020-01-24 07:02:14 -0700

    Added one test to verify that a newly created role can be retrieved

--- m5-lesson1-start/um-webapp/src/test/java/com/baeldung/um/web/role/RoleBasicLiveTest.java
+++ m5-lesson1-start/um-webapp/src/test/java/com/baeldung/um/web/role/RoleBasicLiveTest.java
@@ -7,18 +7,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.List;
-
-import org.hamcrest.Matchers;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
 import com.baeldung.um.persistence.model.Role;
 import com.baeldung.um.spring.CommonTestConfig;
 import com.baeldung.um.spring.UmClientConfig;
@@ -29,30 +17,56 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import java.util.List;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@ActiveProfiles({ CLIENT, TEST })
+@ActiveProfiles({CLIENT, TEST})
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { UmLiveTestConfig.class, UmClientConfig.class, CommonTestConfig.class }, loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = {UmLiveTestConfig.class, UmClientConfig.class,
+    CommonTestConfig.class}, loader = AnnotationConfigContextLoader.class)
 public class RoleBasicLiveTest {
 
-    private final static String JSON = MediaType.APPLICATION_JSON.toString();
-    private final static String URI = "http://localhost:8082/um-webapp/api/roles";
+  private final static String URI = "http://localhost:8082/um-webapp/api/roles";
 
-    @Test
-    public void whenAllRolesAreRetrieved_then200OK(){
-//        final String uri  = "http://localhost:8082/um-webapp/api/roles";
-        final RequestSpecification basicAuth = RestAssured.given().auth().preemptive().basic(Um.ADMIN_EMAIL,Um.ADMIN_PASS);
-        final Response response = basicAuth.accept(ContentType.JSON).get(URI);
+  @Test
+  public void whenAllRolesAreRetrieved_then200OK() {
+    final RequestSpecification basicAuth = RestAssured.given().auth().preemptive()
+        .basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS);
+    final Response response = basicAuth.accept(ContentType.JSON).get(URI);
 
-        Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
-    }
+    Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+  }
 
-    @Test
-    public void whenAllRolesAreRetrieved_thenAtLeastOneRoleExists() {
-        final Response response = RestAssured.given().auth().preemptive().basic(Um.ADMIN_EMAIL,Um.ADMIN_PASS).accept(ContentType.JSON).get(URI);
-        final List<Role> roles = response.as(List.class);
+  @Test
+  public void whenAllRolesAreRetrieved_thenAtLeastOneRoleExists() {
+    final Response response = RestAssured.given().auth().preemptive()
+        .basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS).accept(ContentType.JSON).get(URI);
+    final List<Role> roles = response.as(List.class);
 
-        Assert.assertThat(roles, not(Matchers.empty()));
-    }
+    Assert.assertThat(roles, not(Matchers.empty()));
+  }
+
+  @Test
+  public void whenCreatingANewRole_thenRoleCanBeRetrieved() {
+    final Role newRole = new Role(randomAlphabetic(6), Sets.newHashSet());
+    final RequestSpecification writeAuth = RestAssured.given().auth().preemptive()
+        .basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS);
+    final Response createResponse = writeAuth.contentType(ContentType.JSON).body(newRole).post(URI);
+
+    final String locationHeader = createResponse.getHeader("Location");
+      final RequestSpecification readAuth = RestAssured.given().auth().preemptive()
+          .basic(Um.ADMIN_EMAIL, Um.ADMIN_PASS);
+    final Role retrievedRole = readAuth.accept(ContentType.JSON).get(locationHeader).as(Role.class);
+
+    assertThat(newRole, equalTo(retrievedRole));
+
+  }
 
 }
