dc4969a7238c7f12d66ad167615b7d942d5c8b04
Todd A Nordquist <toddnordquist144@gmail.com>
2020-01-24 13:58:20 -0700

    Added higher level create method in the client.
    We have a full implementation of this CREATE operation, which is now at the much higher level. It no longer returns a response. It actually goes back tothe API, retrieves the new role, and returns that new role. So let's have a look at the implementation. First of all, we are ofcourse doing the CREATE, and we are operating on the response from the API. We are then extracting this location header out of the response. This is what we expect the API will return. So basically, the API gives us all the information we need to discover the newly created resource, and it does so by returning the location of that resource via this location header

--- m5-lesson2-start/um-client/src/test/java/com/baeldung/um/client/template/RoleSimpleApiClient.java
+++ m5-lesson2-start/um-client/src/test/java/com/baeldung/um/client/template/RoleSimpleApiClient.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Role;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
@@ -47,11 +48,17 @@ public final class RoleSimpleApiClient {
   }
 
   public final Response findAllAsResponse() {
-      return read(getUri());
+    return read(getUri());
   }
 
   public final Response createAsResponse(final Role role) {
-      return givenAuthenticated().contentType(JSON).body(role).post(getUri());
+    return givenAuthenticated().contentType(JSON).body(role).post(getUri());
+  }
+
+  public final Role create(final Role role) {
+    final Response response = createAsResponse(role);
+    final String locationOfNewResource = response.getHeader(HttpHeaders.LOCATION);
+    return read(locationOfNewResource).as(Role.class);
   }
 
   // UTIL
