1dc9778be4b05ed9a34c3013c8266463c8e6eb25
Todd A Nordquist <toddnordquist144@gmail.com>
2020-02-04 11:26:45 -0700

    Set up the refresh token configuration.

--- m6-lesson4-start/um-webapp/src/main/java/com/baeldung/um/spring/AuthorizationServerConfiguration.java
+++ m6-lesson4-start/um-webapp/src/main/java/com/baeldung/um/spring/AuthorizationServerConfiguration.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -18,39 +22,52 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+  @Autowired
+  private AuthenticationManager authenticationManager;
 
-    @Value("${signing-key:oui214hmui23o4hm1pui3o2hp4m1o3h2m1o43}")
-    private String signingKey;
+  @Autowired
+  private UserDetailsService userDetailsService;
 
-    public AuthorizationServerConfiguration() {
-        super();
-    }
+  @Value("${signing-key:oui214hmui23o4hm1pui3o2hp4m1o3h2m1o43}")
+  private String signingKey;
 
-    // beans
+  public AuthorizationServerConfiguration() {
+    super();
+  }
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey(signingKey);
-        return jwtAccessTokenConverter;
-    }
+  // beans
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    final JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+    jwtAccessTokenConverter.setSigningKey(signingKey);
+    return jwtAccessTokenConverter;
+  }
 
-    // config
+  @Bean
+  public TokenStore tokenStore() {
+    return new JwtTokenStore(accessTokenConverter());
+  }
 
-    @Override
-    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
-        // @formatter:off
-        clients.inMemory()
+  @Bean
+  @Primary
+  public DefaultTokenServices tokenServices() {
+    final DefaultTokenServices tokenServices = new DefaultTokenServices();
+    tokenServices.setTokenStore(tokenStore());
+    tokenServices.setSupportRefreshToken(true);
+    return tokenServices;
+  }
+
+  // config
+
+  @Override
+  public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+    // @formatter:off
+    clients.inMemory()
         .withClient("live-test")
         .secret("bGl2ZS10ZXN0")
-        .authorizedGrantTypes("password")
+        .authorizedGrantTypes("password", "refresh_token")
+        .refreshTokenValiditySeconds(3600 * 24)
         .scopes("um-webapp", "read", "write", "trust")
         .scopes("um-webapp")
         .autoApprove("um-webapp")
@@ -59,23 +76,24 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         //
         .withClient("um")
         .secret("VXB0YWtlLUlyb24h")
-        .authorizedGrantTypes("password")        
-        .scopes("um-webapp", "read", "write", "trust")        
+        .authorizedGrantTypes("password")
+        .scopes("um-webapp", "read", "write", "trust")
         .autoApprove("um-webapp")
         .accessTokenValiditySeconds(3600)
-        ;
-        // @formatter:on
-    }
+    ;
+    // @formatter:on
+  }
 
-    @Override
-    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
-     // @formatter:off
-     endpoints.
-     tokenStore(tokenStore()).
-     authenticationManager(authenticationManager).
-     allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST).
-     accessTokenConverter(accessTokenConverter());
-  // @formatter:on
-    }
+  @Override
+  public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
+    // @formatter:off
+    endpoints.
+        tokenStore(tokenStore()).
+        authenticationManager(authenticationManager).
+        userDetailsService(userDetailsService).
+        allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST).
+        accessTokenConverter(accessTokenConverter());
+    // @formatter:on
+  }
 
 }
